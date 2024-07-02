88a32423d08477ccf1910cd980b164808b18e375
Adiatma Kamarudin <adiatma.mail@gmail.com>
2024-07-02 23:00:40 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# hello-nodejs
+
+Simple node js serve HTML with NodeJS as REST API.
+
+### Pre-requisites
+
+- Node 21.0
+
+```bash
+chmod +x ./index.js
+./index.js
+```
+
+Open in your browser `http://localhost:5000`
---
+++ index.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+const http = require("http");
+const fs = require("fs");
+
+const getTodo = async () => {
+  const url = `https://jsonplaceholder.typicode.com/todos/1`;
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Ops error occured from server.`);
+  }
+
+  let result = "";
+
+  const reader = response.body.getReader();
+  const decoder = new TextDecoder();
+
+  while (true) {
+    const { done, value } = await reader.read();
+    try {
+      if (done) break;
+      result += decoder.decode(value, { stream: true });
+    } catch (e) { }
+  }
+
+  return result;
+};
+
+(async () => {
+  const server = http.createServer(async (request, response) => {
+    const { url } = request;
+    if (url === "/") {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.end(fs.readFileSync("./index.html"));
+    } else if (url.startsWith("/api")) {
+      response.writeHead(200);
+      const result = await getTodo();
+      response.write(result);
+      response.end();
+    } else {
+      response.writeHead(400);
+      response.end();
+    }
+  });
+
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+  });
+})();
