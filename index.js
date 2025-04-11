cabfa48df4449a05547b83598328ba70af554c0c
Adiatma Kamarudin <adiatma.mail@gmail.com>
2025-04-11 22:16:18 +0700

    chore: update log

--- index.js
+++ index.js
@@ -45,6 +45,6 @@ const getTodo = async () => {
 
   const PORT = process.env.PORT || 5000;
   server.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+    console.log(`Server run on ${PORT}`);
   });
 })();
